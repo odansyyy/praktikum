9ceeb5d6c2670d357f7e27aa7e5c5b18313c27f4
odansyyy <odansyaharta26@gmail.com>
2023-09-29 09:51:49 +0700

    pertama

--- src/App.js
+++ src/App.js
@@ -1,25 +1,218 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+export default function Button() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button>
+      I don't do anything
+    </button>
   );
 }
 
-export default App;
+// export default function Button() {
+//   function handleClick() {
+//     alert('You clicked me!');
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
+//   );
+// }
+
+
+
+// function AlertButton({ message, children }) {
+//   return (
+//     <button onClick={() => alert(message)}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <AlertButton message="Playing!">
+//         Play Movie
+//       </AlertButton>
+//       <AlertButton message="Uploading!">
+//         Upload Image
+//       </AlertButton>
+//     </div>
+//   );
+// }
+
+
+
+// function Button({ onClick, children }) {
+//   return (
+//     <button onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// function PlayButton({ movieName }) {
+//   function handlePlayClick() {
+//     alert(`Playing ${movieName}!`);
+//   }
+
+//   return (
+//     <Button onClick={handlePlayClick}>
+//       Play "{movieName}"
+//     </Button>
+//   );
+// }
+
+// function UploadButton() {
+//   return (
+//     <Button onClick={() => alert('Uploading!')}>
+//       Upload Image
+//     </Button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <PlayButton movieName="Kiki's Delivery Service" />
+//       <UploadButton />
+//     </div>
+//   );
+// }
+
+
+
+// function Button({ onSmash, children }) {
+//   return (
+//     <button onClick={onSmash}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Button onSmash={() => alert('Playing!')}>
+//         Play Movie
+//       </Button>
+//       <Button onSmash={() => alert('Uploading!')}>
+//         Upload Image
+//       </Button>
+//     </div>
+//   );
+// }
+
+
+// export default function App() {
+//   return (
+//     <Toolbar
+//       onPlayMovie={() => alert('Playing!')}
+//       onUploadImage={() => alert('Uploading!')}
+//     />
+//   );
+// }
+
+// function Toolbar({ onPlayMovie, onUploadImage }) {
+//   return (
+//     <div>
+//       <Button onClick={onPlayMovie}>
+//         Play Movie
+//       </Button>
+//       <Button onClick={onUploadImage}>
+//         Upload Image
+//       </Button>
+//     </div>
+//   );
+// }
+
+// function Button({ onClick, children }) {
+//   return (
+//     <button onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div className="Toolbar" onClick={() => {
+//       alert('You clicked on the toolbar!');
+//     }}>
+//       <button onClick={() => alert('Playing!')}>
+//         Play Movie
+//       </button>
+//       <button onClick={() => alert('Uploading!')}>
+//         Upload Image
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default function Signup() {
+//   return (
+//     <form onSubmit={() => alert('Submitting!')}>
+//       <input />
+//       <button>Send</button>
+//     </form>
+//   );
+// }
+
+// export default function Signup() {
+//   return (
+//     <form onSubmit={e => {
+//       e.Propagation();
+//       alert('Submitting!');
+//     }}>
+//       <input />
+//       <button>Send</button>
+//     </form>
+//   );
+// }
+
+// export default function LightSwitch() {
+//   function handleClick() {
+//     let bodyStyle = document.body.style;
+//     if (bodyStyle.backgroundColor === 'black') {
+//       bodyStyle.backgroundColor = 'white';
+//     } else {
+//       bodyStyle.backgroundColor = 'black';
+//     }
+//   }
+
+//   return (
+//     <button onClick={handleClick()}>
+//       Toggle the lights
+//     </button>
+//   );
+// }
+
+
